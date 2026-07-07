23ed10e81c48bd2994fec91735905251136a76ad
Tony <anthonykanogo@gmail.com>
2026-07-07 22:41:06 +0300

    Add basic SVG reader

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ filename = "input/square.svg"
 
 paths, attributes = svg2paths(filename)
 
-print("--------------------------------")
-print("TonyCAM SVG Reader")
-print("--------------------------------")
-print(f"File : {filename}")
-print(f"Number of paths : {len(paths)}")
+print("=" * 45)
+print("TonyCAM Geometry Inspector")
+print("=" * 45)
 
-for i, path in enumerate(paths):
-    print(f"\nPath {i+1}")
-    print(path)
+for path_number, path in enumerate(paths, start=1):
+
+    print(f"\nPATH {path_number}")
+
+    for segment_number, segment in enumerate(path, start=1):
+
+        start = segment.start
+        end = segment.end
+
+        print(f"\nSegment {segment_number}")
+        print(f"Type : {type(segment).__name__}")
+        print(f"Start: ({start.real:.2f}, {start.imag:.2f})")
+        print(f"End  : ({end.real:.2f}, {end.imag:.2f})")
