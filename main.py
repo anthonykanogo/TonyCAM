5fac2c7a7290e5ba9a029ad75deb0b31a38c0fd3
Tony <anthonykanogo@gmail.com>
2026-07-07 22:36:00 +0300

    Add basic SVG reader

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Welcome to TonyCAM!")
-print("SVG → HyCut CNC G-code Generator")
+from svgpathtools import svg2paths
+
+filename = "input/square.svg"
+
+paths, attributes = svg2paths(filename)
+
+print("--------------------------------")
+print("TonyCAM SVG Reader")
+print("--------------------------------")
+print(f"File : {filename}")
+print(f"Number of paths : {len(paths)}")
+
+for i, path in enumerate(paths):
+    print(f"\nPath {i+1}")
+    print(path)
