f5f7abb27c16b4abae7fb72ef156315bc8104dc5
Tony <anthonykanogo@gmail.com>
2026-07-08 19:47:53 +0300

    Add batch SVG processing

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
+import os
+
 from tonycam.svg_reader import read_svg
 from tonycam.gcode_writer import generate_gcode
 from tonycam.postprocessors.hycut import save_gcode
 
 
-SVG_FILE = "input/square.svg"
-OUTPUT_FILE = "output/square.cnc"
+INPUT_FOLDER = "input"
+OUTPUT_FOLDER = "output"
 
 
-paths = read_svg(SVG_FILE)
+# Find all SVG files
+svg_files = []
 
-gcode = generate_gcode(paths)
+for filename in os.listdir(INPUT_FOLDER):
+    if filename.endswith(".svg"):
+        svg_files.append(filename)
 
-save_gcode(gcode, OUTPUT_FILE)
 
 print("TonyCAM")
 print("--------------------")
-print(f"Loaded : {SVG_FILE}")
-print(f"Paths  : {len(paths)}")
-print(f"Saved  : {OUTPUT_FILE}")
+print(f"Found {len(svg_files)} SVG files")
+
+
+# Process each SVG file
+for svg_file in svg_files:
+
+    input_path = os.path.join(INPUT_FOLDER, svg_file)
+
+    name_without_extension = os.path.splitext(svg_file)[0]
+
+    output_path = os.path.join(
+        OUTPUT_FOLDER,
+        name_without_extension + ".cnc"
+    )
+
+
+    paths = read_svg(input_path)
+
+    gcode = generate_gcode(paths)
+
+    save_gcode(gcode, output_path)
+
+
+    print(f"Processed: {svg_file}")
+    print(f"Saved: {output_path}")
