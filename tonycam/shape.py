93efc4d2592745d94b3afac5ba9be17e3464f861
Tony <anthonykanogo@gmail.com>
2026-07-09 20:32:19 +0300

    Add circle shape recognition

--- tonycam/shape.py
+++ tonycam/shape.py
@@ -2,6 +2,22 @@ def classify_shape(path):
 
     segments = len(path)
 
+    # Check for circles
+    if segments == 2:
+
+        arc_count = 0
+
+        for segment in path:
+
+            if segment.__class__.__name__ == "Arc":
+                arc_count += 1
+
+        if arc_count == 2:
+            return "Circle"
+
+
+    # Polygon classification
+
     if segments == 3:
         return "Triangle"
 
